949a3ddb7734c5d1135cc280d351adea5f21b4ba
Dilek Baykara <baykara.d@gmail.com>
2024-05-11 23:57:06 -0400

    added images and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Img1 from ".././src/images/img-1.jpg";
+import Img2 from ".././src/images/img-2.jpg";
+import Img3 from ".././src/images/img-3.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header-div">
+        <header className="App-header">
+          <div id="title-div">Dilek Baykara</div>
+          <div id="button-div">
+            <button>SKETCH</button>
+            <button>ABOUT</button>
+          </div>
+        </header>
+        <div id="sketch-div">
+          <img alt="sketch-1" src={Img1}></img>
+          <img alt="sketch-2" src={Img2}></img>
+          <img alt="sketch-3" src={Img3}></img>
+        </div>
+      </div>
     </div>
   );
 }
