0b90beff13c240cc29c6e888c243a4f6e555a581
Dilek Baykara <baykara.d@gmail.com>
2024-07-04 16:12:57 -0400

    styled about page

--- src/About.js
+++ src/About.js
@@ -3,31 +3,49 @@ import React from "react";
 function About() {
   return (
     <div id="about-div">
-      <div id="about-title">About</div>
-
+      <div id="about-title">ABOUT</div>
+      <br></br>
       <div id="name">
-        <li>Dilek Baykara</li>
-        <li>info@dilekbaykara.com</li>
+        <div>Dilek Baykara</div>
+
+        <div>info@dilekbaykara.com</div>
       </div>
+      <br></br>
+      <div id="ex-title">Solo/Group Exhibitions</div>
 
       <div id="ex-div">
-        <li>2016 Summers Ghost, Parlor Gallery, Asbury Park, NJ</li>
-        <li>2013 – 2016 Tiny Trifecta, Cotton Candy Machine, Brooklyn, NY </li>
-        <li>2014 Wait For The Moon, Arch Enemy Arts, Philadelphia, PA </li>
-        <liv>2014 Grand Opening, SuperChief Gallery LA, Los Angeles, CA</liv>
-        <li>2014 CCMxSF, Cotton Candy Machine, Brooklyn, NY</li>
-        <li>2014 Last Rites, Last Rites Gallery, New York, NY</li>
-        <li>
+        <div>2016 Summers Ghost, Parlor Gallery, Asbury Park, NJ</div>
+        <div>
+          2013 – 2016 Tiny Trifecta, Cotton Candy Machine, Brooklyn, NY{" "}
+        </div>
+        <div>2014 Wait For The Moon, Arch Enemy Arts, Philadelphia, PA </div>
+        <div>2014 Grand Opening, SuperChief Gallery LA, Los Angeles, CA</div>
+        <div>2014 CCMxSF, Cotton Candy Machine, Brooklyn, NY</div>
+        <div>2014 Last Rites, Last Rites Gallery, New York, NY</div>
+        <div>
           2013 Jonathan LeVine Juried Show, Parlor Gallery, Asbury Park, NJ
-        </li>
-        <li>2013 Allegorical Quarter, Cotton Candy Machine, Brooklyn, NY</li>{" "}
-        <li>2013 Poster Child, Bushwick Print Lab, Brooklyn, NY</li>
-        <li>2013 Peripeteia II, Skylight Gallery, New York, NY</li>
-        <li>2013 Black On High, The Gallery, Los Angeles, CA</li>
-        <li>2013 Solo Show, Culturefix Gallery, New York, NY</li>
-        <li>2013 Society of Illustrators, New York, NY</li>
-        <li>2013 SVA Thesis Show 2013, SVA Gallery, New York, NY</li>
-        <li>2012 Summer Spell, Gallery 309, Philadelphia, PA</li>
+        </div>
+        <div>2013 Allegorical Quarter, Cotton Candy Machine, Brooklyn, NY</div>{" "}
+        <div>2013 Poster Child, Bushwick Print Lab, Brooklyn, NY</div>
+        <div>2013 Peripeteia II, Skylight Gallery, New York, NY</div>
+        <div>2013 Black On High, The Gallery, Los Angeles, CA</div>
+        <div>2013 Solo Show, Culturefix Gallery, New York, NY</div>
+        <div>2013 Society of Illustrators, New York, NY</div>
+        <div>2013 SVA Thesis Show 2013, SVA Gallery, New York, NY</div>
+        <div>2012 Summer Spell, Gallery 309, Philadelphia, PA</div>
+      </div>
+      <br></br>
+
+      <div id="web-title">Selected Publications and Web Features</div>
+
+      <div id="web-div">
+        <div>2015 Uglier Dirtier Nastier Noisier</div>{" "}
+        <div>22014 Ugly Dirty Nasty Noisy</div>
+        <div>2014 Village Voice, Best of NYC 2014</div>
+        <div>2013 Juxtapoz Magazine, Feature</div>
+        <div>2013 Bust Magazine, Feature</div>
+        <div>2013 Mass Appeal, Interview</div>
+        <div>2013 Vice, Featured Artist</div>
       </div>
     </div>
   );
